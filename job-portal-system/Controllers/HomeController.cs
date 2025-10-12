f3fd029138b37c5bb2e573f5355b5908fdc9be86
AHMED\ahmed <ahmedrooshdii@gmail.com>
2025-10-13 00:22:20 +0300

    Add ASP.NET Core MVC project structure

---
+++ job-portal-system/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using job_portal_system.Models;
+
+namespace job_portal_system.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
